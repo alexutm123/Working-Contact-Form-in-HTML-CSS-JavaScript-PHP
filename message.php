19e8be74e72dd6ef7555341158bdb8a910f0219e
alexutm123 <pilich.sasha@gmail.com>
2024-03-11 19:13:11 +0300

    FInal

--- message.php
+++ message.php
@@ -1,15 +1,26 @@
 <?php
-// $name=$_POST['name'];
-// $email=$_POST['email'];
-// $phone=$_POST['phone'];
-// $webside=$_POST['website'];
-// $message=$_POST['message'];
-// if(!empty($email) && !empty($message)){ //if email and message field is not empty
- 
-// }
-// else{ 
-//     echo "Email and password field is required!";
-// }
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$website = $_POST['website'];
+$message = $_POST['message'];
 
-echo "TEST";
+if (!empty($email) && !empty($message)) { // если поля email и сообщение не пустые
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // если email валидный
+        $receiver = "pilich.sasha@gmail.com        "; // email адрес получателя
+        $subject = "From: $name <$email>"; // тема письма
+        $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\nMessage: $message\n\nRegards,\n$name"; // текст письма
+        $sender = "From: $email"; // отправитель
+
+        if (mail($receiver, $subject, $body, $sender)) { // отправка письма
+            echo "Your message has been sent";
+        } else {
+            echo "Sorry, failed to send your message!";
+        }
+    } else {
+        echo "Enter a valid email address!";
+    }
+} else { 
+    echo "Email and message fields are required!";
+}
 ?>
