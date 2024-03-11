cd2524d2fc282e98a505a2a33b9c2097430c761c
alexutm123 <pilich.sasha@gmail.com>
2024-03-11 17:56:46 +0300

    test

---
+++ message.php
@@ -0,0 +1,15 @@
+<?php
+// $name=$_POST['name'];
+// $email=$_POST['email'];
+// $phone=$_POST['phone'];
+// $webside=$_POST['website'];
+// $message=$_POST['message'];
+// if(!empty($email) && !empty($message)){ //if email and message field is not empty
+ 
+// }
+// else{ 
+//     echo "Email and password field is required!";
+// }
+
+echo "TEST";
+?>
